912046021ba4ba8c42855336389f3aef9bc3408b
tako0910 <tako.oct0910@gmail.com>
2014-02-22 19:05:20 +0900

    m7wlj: modify for led

--- include/linux/mfd/pm8xxx/vibrator.h
+++ include/linux/mfd/pm8xxx/vibrator.h
@@ -32,6 +32,8 @@ struct pm8xxx_vibrator_platform_data {
 	int initial_vibrate_ms;
 	int max_timeout_ms;
 	int level_mV;
+	void (*camera_cb) (void);
+	void (*camera_off_cb) (void);
 };
 
 int pm8xxx_vibrator_config(struct pm8xxx_vib_config *vib_config);
